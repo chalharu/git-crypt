d3c4709f1e046485bdda2dbe8fa8059dbc2997fe
chalharu <1978012+chalharu@users.noreply.github.com>
2026-02-04 06:58:00 +0900

    feat: build_gitattributes関数での相対パス取得時に無視すべきファイルをチェックする処理を追加

--- src/main.rs
+++ src/main.rs
@@ -2168,7 +2168,9 @@ fn build_gitattributes<P: AsRef<Path>>(
     ];
 
     for path in additional_paths {
-        if let Ok(relative_path) = relative_git_path(repo, path.as_ref()) {
+        if let Ok(relative_path) = relative_git_path(repo, path.as_ref())
+            && repo.repo.status_should_ignore(&relative_path)?
+        {
             special_files.push(relative_path.as_os_str().as_bytes().to_vec());
         }
     }
