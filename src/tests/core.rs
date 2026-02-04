147c9c331eda2756ee9f7e724099e7bd3d453750
chalharu <1978012+chalharu@users.noreply.github.com>
2026-02-05 06:23:44 +0900

    test: encrypt_decryptの正常系テストを追加

--- src/tests/core.rs
+++ src/tests/core.rs
@@ -1,6 +1,29 @@
 use std::{path::PathBuf, str::FromStr};
 
-use crate::{encrypt, tests::util::TestRepository};
+use crate::{decrypt, encrypt, tests::util::TestRepository};
+
+// Given: 平文 b"hello\n", 対象パス src/a.txt
+// When.1: encrypt()
+// Then.1: 返るOIDが平文とは異なる
+// When.2: decrypt()
+// Then.2: 返る内容が平文と同一
+#[test]
+fn encrypt_decrypt_正常系() {
+    let mut repo = TestRepository::new();
+    let input_path = PathBuf::from_str("src/a.txt").unwrap();
+    let input_content = b"hello\n";
+    let input_oid = repo.context().repo.repo.blob(input_content).unwrap();
+    let encrypted_oid = encrypt(repo.context_mut(), input_oid, input_path.as_os_str()).unwrap();
+    assert_ne!(input_oid, encrypted_oid);
+
+    let decrypted_content = decrypt(
+        repo.context_mut(),
+        encrypted_oid,
+        input_path.as_os_str().as_encoded_bytes(),
+    )
+    .unwrap();
+    assert_eq!(input_oid, decrypted_content);
+}
 
 // Given: EncryptionPolicy の regex が ^src/、入力パス docs/readme.md
 // When: encrypt()
