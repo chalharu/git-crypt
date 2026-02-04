79412ecd2bbba1e31cc556f509d990ac5b5d519c
chalharu <1978012+chalharu@users.noreply.github.com>
2026-02-04 17:04:30 +0900

    fix: テストでのrepoメソッドの呼び出しを修正

--- src/tests/core.rs
+++ src/tests/core.rs
@@ -9,7 +9,7 @@ use crate::{encrypt, tests::util::TestRepository};
 fn 暗号化対象外は素通し() {
     let mut repo = TestRepository::new();
     let input_path = PathBuf::from_str("docs/readme.md").unwrap();
-    let input_oid = repo.context().repo().repo.blob(b"Hello, World!").unwrap();
+    let input_oid = repo.context().repo.repo.blob(b"Hello, World!").unwrap();
     let output_oid = encrypt(repo.context_mut(), input_oid, input_path.as_os_str()).unwrap();
     assert_eq!(input_oid, output_oid);
 }
