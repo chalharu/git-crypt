12d89081b6fd9e12c275b9de15d7c64d4bd52e0e
chalharu <1978012+chalharu@users.noreply.github.com>
2026-02-05 15:38:20 +0900

    feat: cleanおよびsmudge関数の復号キャッシュなしテストを追加

--- src/tests/clean_and_smudge.rs
+++ src/tests/clean_and_smudge.rs
@@ -1,6 +1,6 @@
 use std::{path::PathBuf, str::FromStr as _};
 
-use crate::{clean, smudge, tests::util::TestRepositoryBuilder};
+use crate::{clean, smudge, tests::util::{TestRepositoryBuilder, generate_keypair}};
 
 // 平文→暗号化→復号で一致
 // Given: 平文 b"hello\n", 対象パス src/a.txt
@@ -34,4 +34,46 @@ fn clean_smudge_正常系() {
     .unwrap();
     assert_eq!(input_content, decrypted_content.as_slice());
     drop(repo);
+}
+
+// 平文→暗号化→復号(キャッシュなし)で一致
+// Given: 平文 b"hello\n", 対象パス src/a.txt
+// When.1: clean()
+// Then.1: 返る内容が平文とは異なる
+// When.2: smudge()
+// Then.2: 返る内容が平文と同一
+#[test]
+fn clean_smudge_復号キャッシュなし() {
+    let keypair = generate_keypair();
+    let repo = TestRepositoryBuilder::new()
+        .with_private_key_data(keypair.0.clone())
+        .with_public_key_data(keypair.1.clone())
+        .build();
+    let input_path = PathBuf::from_str("src/a.txt").unwrap();
+    let input_content = b"hello\n";
+    let mut encrypted_content = Vec::new();
+    clean(
+        &mut input_content.as_slice(),
+        &mut encrypted_content,
+        input_path.as_os_str(),
+        repo.path(),
+    )
+    .unwrap();
+    assert_ne!(input_content, encrypted_content.as_slice());
+    drop(repo);
+
+    // キャッシュの影響を受けないようにするためリポジトリを新規作成しなおす
+    let repo = TestRepositoryBuilder::new()
+        .with_private_key_data(keypair.0.clone())
+        .with_public_key_data(keypair.1.clone())
+        .build();
+    let mut decrypted_content = Vec::new();
+    smudge(
+        &mut encrypted_content.as_slice(),
+        &mut decrypted_content,
+        input_path.as_os_str(),
+        repo.path(),
+    )
+    .unwrap();
+    assert_eq!(input_content, decrypted_content.as_slice());
 }
